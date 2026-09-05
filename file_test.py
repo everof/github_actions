2dc2aadc69b720983f12845d395aeac0254408d4
Ubuntu <ubuntu@ip-172-31-22-243.eu-west-1.compute.internal>
2026-09-05 14:32:00 +0000

    spacing improved

--- file_test.py
+++ file_test.py
@@ -5,15 +5,18 @@ def test_calc_addition():
     output = 2 + 4
     assert output == 6
 
+
 def test_calc_subtraction():
     # Function tests the output of 2 - 4
     output = 2 - 4
     assert output == -2
 
+
 def test_calc_multiplication():
     # Function tests the output of 2 * 4
     output = 2 * 4
     assert output == 8
+
 
 def test_coucou():
     # Function tests whatever else
